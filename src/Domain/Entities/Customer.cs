cc2edf71fba34e4674f8adc4d8bf217a35eb76ad
Juan Pablo Canseco Rios <juan.canseco.rios@gmail.com>
2024-02-28 18:53:47 -0600

    Add create constructor and Update method in Customer Entity

--- src/Domain/Entities/Customer.cs
+++ src/Domain/Entities/Customer.cs
@@ -7,10 +7,23 @@ public class Customer : BaseAuditableEntity<int>
     public ContactPerson ContactPerson { get; private set; }
     public Address Address { get; private set; }
 
+    public void Update(ContactPerson contactPerson, Address address)
+    {
+        ContactPerson = contactPerson;
+        Address = address;
+    }
+
     private Customer()
     {
         IdentificationCode = default!;
         ContactPerson = default!;
         Address = default!;
+    }
+
+    public Customer(string identificationCode, ContactPerson contactPerson, Address address)
+    {
+        IdentificationCode = identificationCode;
+        ContactPerson = contactPerson;
+        Address = address;
     }
 }
